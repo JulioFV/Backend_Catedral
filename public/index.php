72ae5992bf7927971090398b64f1b58bb4ce638e
JulioFV <21011178@itsoeh.edu.mx>
2026-08-19 10:24:46 -0600

    Integración de headers principales de CORS

--- public/index.php
+++ public/index.php
@@ -7,7 +7,18 @@ use src\config\EnvLoader;
 
 EnvLoader::load();
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit();
+}
 $router = new Router();
+
+
 
 RouteRegistrar::register($router);
 
